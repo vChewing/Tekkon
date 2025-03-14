87878ca7504b17c714c13c5db0b4aacf52e7ea95
ShikiSuen <shikisuen@outlook.com>
2025-03-14 23:33:38 +0800

    Update license years.

--- Sources/Tekkon/Tekkon_Chopper.swift
+++ Sources/Tekkon/Tekkon_Chopper.swift
@@ -1,4 +1,4 @@
-// (c) 2025 and onwards The vChewing Project (LGPL v3.0 License or later).
+// (c) 2022 and onwards The vChewing Project (LGPL v3.0 License or later).
 // ====================
 // This code is released under the SPDX-License-Identifier: `LGPL-3.0-or-later`.
 
